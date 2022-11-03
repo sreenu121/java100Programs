cc60aedf56826201b715ca52656377f9e4a3c087
sreenivasulu atthipati <101385784+sreenu121@users.noreply.github.com>
2022-11-03 15:11:12 +0530

    taken the inputs
    given inputs from the user

---
+++ Scanner1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Scanner1{
+	public static void main(String []args){
+		int a;
+		float b;
+		double c;
+		String x;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the integer number");
+		a = sc.nextInt();
+		System.out.println("your enterd interger number"+a);
+		System.out.println("enter the float number ");
+		b=sc.nextFloat();
+		System.out.println("your enterd float number"+b);
+		System.out.println("Enter the double value");
+		c = sc.nextDouble();
+		System.out.println("your entered double value"+c);
+		System.out.println("Enter the String value");
+		x = sc.nextLine();
+		System.out.println("Entered String value"+x);
+		
+		
+	}
+}
