37815e6fe81cb5c169ee4462d62fdde88bc2d5a3
sreenivasulu atthipati <101385784+sreenu121@users.noreply.github.com>
2022-11-03 15:07:20 +0530

    Hello
    fist java program

---
+++ Hello.java
@@ -0,0 +1,8 @@
+class Hello{
+	public static void main(String []args){
+	System.out.println("Hello world by sreenu work's space");
+	for (int c =0; c <= 10; c++){
+	System.out.println(c);
+	}
+   }
+  }
